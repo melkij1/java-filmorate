4339b0cd783817cc1dbfa04abcc5f386652298b1
mmsobakin <mmsobakin@spasibosb.ru>
2024-08-19 14:31:48 +0300

    update handler v3.2

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -8,26 +8,19 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exception.ErrorResponse;
-import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exception.*;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse validateException(final MethodArgumentNotValidException e) {
-        log.error(e.getMessage());
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> validationException(final ConstraintViolationException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
+
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -48,6 +41,21 @@ public class ErrorHandler {
     public ErrorResponse internalServerError(final Exception e) {
         log.error(e.getMessage());
         return new ErrorResponse("Произошла внутренняя ошибка сервера");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse userNotFoundDeleteException(final UserNotFoundDeleteException e) {
+        log.error(e.getMessage());
+        return new ErrorResponse("Не найден пользователь при удалении");
+    }
+
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse validateException(final ValidationException e) {
+        log.error(e.getMessage());
+        return new ErrorResponse(e.getMessage());
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,20 +2,21 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.exception.FilmNotFoundException;
-import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
+import ru.yandex.practicum.filmorate.exception.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class FilmService {
     private final FilmStorage filmStorage;
-    private final UserService userService;
+    private final UserStorage userStorage;
 
     public List<Film> findAll() {
         return filmStorage.findAll();
@@ -41,13 +42,23 @@ public class FilmService {
     }
 
     public void removeLike(int filmId, int userId) {
-        final Film film = findById(filmId);
-        final User user = userService.findById(userId);
-
-        if (!film.getLikes().contains(userId)) {
-            throw new UserNotFoundException("Пользователь не ставил лайк");
+        Optional<Film> film = filmStorage.findById(filmId);
+        if (film.isEmpty()) {
+            throw new FilmNotFoundException("Не найден фильм, у которого удаляется лайк");
+        } else if (userStorage.findById(userId).isEmpty()) {
+            throw new UserNotFoundException("Не найден пользователь, чей удаляется лайк");
+        } else if (filmId < 0 || userId < 0) {
+            throw new ValidationException("Некорректный формат переданных параметров");
+        } else {
+            film.get().getLikes().remove(userId);
         }
-        film.getLikes().remove(userId);
+//        final Film film = findById(filmId);
+//        final User user = userService.findById(userId);
+//
+//        if (!film.getLikes().contains(userId)) {
+//            throw new UserNotFoundException("Пользователь не ставил лайк");
+//        }
+//        film.getLikes().remove(userId);
     }
 
     public List<Film> findPopular(int count) {
